ffb7b6bfa5de8a897355000e12a4795cdfb44f1f
NastyaGavrichenkova <lady_markos@mail.ru>
2023-08-22 23:00:46 +0800

    changes for zip test

--- tests/test_zip.py
+++ tests/test_zip.py
@@ -2,15 +2,18 @@ import zipfile
 import os
 from conftest import RESOURCES_DIR
 
+files_to_zip = ['docs-pytest-org-en-latest.pdf',
+                'file_example_XLS_10.xls',
+                'file_example_XLSX_50.xlsx']
+
 
 def test_zip_file():
-    with zipfile.ZipFile(os.path.join(RESOURCES_DIR, 'new_file.zip')) as zip_file:
-        name_list = zip_file.namelist()
-        zip_file.extract('new_file.txt', path=RESOURCES_DIR)
-        text = zip_file.read('new_file.txt')
-        print(text)
+    with zipfile.ZipFile(os.path.join(RESOURCES_DIR, 'resourses.zip'), 'w') as zip_folder:
+        for file_to_zip in files_to_zip:
+            file_path = os.path.join(RESOURCES_DIR, file_to_zip)
+            zip_folder.write(file_path, file_to_zip)
+        name_list = zip_folder.namelist()
 
-    assert 'new_file.txt' in name_list
-    assert text == b"I'm a new file for test zip"
+    assert name_list == files_to_zip
 
-    os.remove(os.path.join(RESOURCES_DIR, 'new_file.txt'))
+    os.remove(os.path.join(RESOURCES_DIR, 'resourses.zip'))
